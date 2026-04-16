b8fe460fee2038fd9c7b9e404856df218c8fd506
Joshua-Swinburne <105350111@student.swin.edu.au>
2026-04-16 17:02:16 +1000

    Created folder to hold all project work. Wrote shortened version of trajdeleter script. Wrote skeleton bash scripts to be used later. Wrote CQL TrajDeleter script.

--- p38_project_work/cql_trajdeleter_run_script.sh
+++ p38_project_work/cql_trajdeleter_run_script.sh
@@ -17,8 +17,8 @@ apptainer exec --nv --writable-tmpfs --fakeroot \
   contains/image.sif \
   bash -lc '
     cd unlearning
-    python mujoco_trajdeleter_shortened.py --dataset hopper-medium-expert-v0 --seed 42 --gpu 0 --unlearning_rate 0.11 --model ./Fully_trained_agents/hopper-medium-expert-v0/CQL/params.json
-    --model_params ./Fully_trained_agents/hopper-medium-expert-v0/CQL/model.pt --number_of_unlearning 0.11
+    python mujoco_trajdeleter_shortened.py --dataset hopper-medium-expert-v0 --seed 42 --gpu 0 --unlearning_rate 0.11 --model ./Fully_trained/hopper-medium-expert-v0/CQL/params.json
+    --model_params ./Fully_trained/hopper-medium-expert-v0/CQL/model.pt --number_of_unlearning 0.11
   '
 # This script handles unlearning for one model and one dataset, manually.
 # This isn't particularly efficient, this is just to gauge how many jobs must be run simultaneously to do all the basleines in one shot to massively cut back on time.
